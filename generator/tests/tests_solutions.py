f823775480e38ad2352f902057c825d4a01da813
Erik Sanchez <erik.sape@gmail.com>
2020-08-22 09:55:05 -0500

    test(nqueen): down the number of test from 8 to 15

--- generator/tests/tests_solutions.py
+++ generator/tests/tests_solutions.py
@@ -5,9 +5,9 @@ from known_solutions import solutions
 class TestSolutions(object):
     def test_with_known_solutions(self):
         """
-        Test from 8 to 16
+        Test from 8 to 15
         """
-        for i in range(8, 17):
+        for i in range(8, 16):
             solution = solutions[i-1]
             board = ChessBoard(solution['dimension'], True)
             board.n_queens()
